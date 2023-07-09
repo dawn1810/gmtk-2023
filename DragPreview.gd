cab8c328790132b24b8ff0c790f39853c5053f70
rurimeiko <chandoralong@gmail.com>
2023-07-09 08:23:08 +0700

    deleted:    DragPreview.gd 	modified:   code/Character.gd 	deleted:    screen/TestTrap.gd 	modified:   screen/charater.tscn 	modified:   screen/drag_preview.tscn 	modified:   screen/main.tscn 	code/Camera2D.gd 	code/DragPreview.gd 	code/TestTrap.gd 	screen/item_physic.tscn 	screen/matdat.tscn

--- DragPreview.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite2D
-
-
-func _process(delta):
-	global_position = get_global_mouse_position()
-	
-	if Input.is_action_just_released('LeftMouse'):
-		queue_free()
--- code/Character.gd
+++ code/Character.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 50
+const SPEED = 10
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var jump = true
 func _ready():
 	$AnimationPlayer.play("idle")
 
@@ -14,10 +14,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	
 	velocity.x = 10 * SPEED
-
-
 	move_and_slide()
 
 func dead():
@@ -26,4 +23,11 @@ func dead():
 
 
 func _on_trap_body_entered(body):
+#	$"../Camera2D". = 0
+
+	dead()
+
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited():
 	dead()
