dd9cb470a2add0893d47387e7d4151fd4423813a
dawn1810 <106864922+dawn1810@users.noreply.github.com>
2023-07-09 13:48:58 +0700

    a

--- code/CollectedItem.gd
+++ code/CollectedItem.gd
@@ -4,15 +4,23 @@ extends Area2D
 @export var number : int
 
 var mouse_inside_area = false 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _ready():
-	pass
 
 func _process(delta):
 	if Input.is_action_just_released("LeftMouse") && mouse_inside_area:
+<<<<<<< Updated upstream
 		get_parent().get_node('FrontLayer/Inventory/Control/HBoxContainer/CenterContainer/Item').texture_normal = load(read_json_file('res://code/item.json')[item_name])
 		queue_free()
 	
+=======
+<<<<<<< HEAD
+		get_parent().get_parent().get_node('FrontLayer/Inventory/Control/HBoxContainer/CenterContainer/Item').texture_normal = load(read_json_file('res://code/item.json')[item_name])
+
+=======
+		get_parent().get_node('FrontLayer/Inventory/Control/HBoxContainer/CenterContainer/Item').texture_normal = load(read_json_file('res://code/item.json')[item_name])
+		queue_free()
+	
+>>>>>>> 0e3515cd03b15bbc278912ee4527338c97de3cce
+>>>>>>> Stashed changes
 func read_json_file(file_path):
 	var json_as_text = FileAccess.get_file_as_string(file_path)
 	var json_as_dict = JSON.parse_string(json_as_text)
